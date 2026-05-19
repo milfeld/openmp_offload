1414ba8f5f71c1334ef9f61bdeda9a666a02971b
milfeld <kent.milfeld@gmail.com>
2026-05-19 09:26:57 -0500

    removed declare target around d_saxpy().

--- advanced/6_variant/saxpy.c
+++ advanced/6_variant/saxpy.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define N 100000 
 
 void d_saxpy(int n, float a, float* x, float* y);
 
 #pragma omp declare variant(d_saxpy) match( construct={dispatch} )
 void saxpy(int n, float a, float* x, float* y)
 {
+  printf(" saxpy\n");
   #pragma omp parallel for
-  for(int i=0; i<n; i++) y[i]=a*x[i]+y[i];
+  for(int i=0; i<n; i++) y[i]=9*y[i];
 }
 
-#pragma omp begin declare target
 void d_saxpy(int n, float a, float* x, float* y)
 {
-  #pragma omp target teams distribute parallel for 
-  for(int i=0; i<n; i++) y[i]=a*x[i]+y[i];
+    printf(" d_saxpy\n");
+    #pragma omp target teams distribute parallel for is_device_ptr(x,y)
+    for(int i=0; i<n; i++) y[i]=a*x[i]+y[i];
 }
-#pragma omp end declare target
 
+#define N 100000 
+void    saxpy(int n, float a, float* x, float* y);
+void  d_saxpy(int n, float a, float* x, float* y);
 
 int main(){
    float a=2.0;
@@ -32,11 +34,12 @@ int main(){
 
    #pragma omp target data  map(to: x[0:N]) map(tofrom: y[0:N]) use_device_ptr(x,y)
    {
-      #pragma omp dispatch is_device_ptr(x,y)
-      saxpy(N,a,x,y);
+     #pragma omp dispatch
+     saxpy(N,a,x,y);
    }
+
    for( int i=0; i<N; i++){ 
-     if(  abs(y[i]-2.0)>0.000001 ){printf(" ERROR: val=%f (2)\n",y[i]); return 1;}
+     if(  fabs(y[i]-2.0)>0.000001 ){printf(" ERROR: val=%f (2)\n",y[i]); return 1;}
    }
    printf(" PASS\n");
 }
--- advanced/6_variant/simp_variant.c
+++ advanced/6_variant/simp_variant.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include   <omp.h>
 
-void d_fun();
+void d_fun(void);
 //                 use this v---v  when  v--------------v
-#pragma omp declare variant(d_fun) match(construct={dispatch})
+#pragma omp declare variant(d_fun)   match(construct={dispatch})
 void fun(){                                 //< base function
   printf("   fun: ON_HOST=%c T=true\n", \
         (omp_is_initial_device()==0) ? 'T':'F');
 }
 
 // use this variant when found is in a dispatch construct
-#pragma declare target
 void d_fun(){
   #pragma omp target
   printf(" d_fun: ON_DEV =%c T=true\n", \
         (omp_is_initial_device()==0) ? 'T':'F');
 }
-#pragma end declare target
 
 
 int main(){
